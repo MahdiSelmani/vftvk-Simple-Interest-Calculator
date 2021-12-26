ec5df4980b55e63a647f44d4cbf3b2c954190f84
MahdiSelmani <92759993+MahdiSelmani@users.noreply.github.com>
2021-12-26 15:49:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
     
+    ratevalue = document.getElementById("ratevalue");
+    amount = document.getElementById("amount").value;
+    years = document.getElementById("years").value;
+    rate = document.getElementById("rate").value;
+    result = document.getElementById("value");
+    var interest = 0;
+    var year = parseInt(years) + parseInt(2020);
+    interest = (amount * years * rate)/100;
+    if (amount <= 0) { alert('Enter a positive number') } 
+    else {    result.innerHTML = "If you deposit <mark>"+ amount +"</mark>, <br> at an interest of <mark>" + rate + "%</mark>.<br> You will receive an amount of <mark>" + interest + "</mark>,<br> in the year <mark>" + year +"</mark>";
 }
-        
+}
+function showVal(rate) {
+    ratevalue.innerText = rate+"%";
+}
+
